2b6db459c8f50ff1212a6aee7b60f462728565ef
alvannwanorim <alvannwanorim@gmail.com>
2022-07-12 16:56:19 +0100

    frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
 	production: false,
 	appName: 'Udagram',
-	apiHost: 'http://localhost:8080/api/v0'
+	apiHost: 'http://a96124c416bd24f68b01cf4f9e1c2dce-381895391.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
