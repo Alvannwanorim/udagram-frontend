f1983e99358b040b252d817c1cd0328c6bf44fa4
alvannwanorim <alvannwanorim@gmail.com>
2022-07-12 16:52:34 +0100

    frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+	production: false,
+	appName: 'Udagram',
+	apiHost: 'http://a96124c416bd24f68b01cf4f9e1c2dce-381895391.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
